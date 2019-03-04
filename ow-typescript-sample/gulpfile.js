a85517a623cde22eb2673e9c37d77e2170f6fc5d
James Lockhart <james.lockhart@sykescottages.co.uk>
2019-03-04 22:43:14 +0000

    Added watch for the gulp and a fix for the typings for getInfo

--- ow-typescript-sample/gulpfile.js
+++ ow-typescript-sample/gulpfile.js
@@ -1,46 +1,61 @@
 var gulp = require("gulp");
-var browserify = require("browserify");
-var ts = require("gulp-typescript");
+var watch = require("gulp-watch");
+var sass = require('gulp-sass');
 var source = require('vinyl-source-stream');
+var browserify = require("browserify");
 var tsify = require("tsify");
+
 var paths = {
-  pages: ['src/**/*.html'],
-  css: ['src/css/*.css'],
-  assets: ['desktop-icon.ico', 'IconMouseNormal.png', 'IconMouseOver.png', 'manifest.json']
+    pages: [
+        'src/**/*.html'
+    ],
+    scss: [
+        'src/scss/*.scss'
+    ],
+    assets: [
+        'assets/**/*.*'
+    ],
+    typescript: [
+        'src/**/*.ts'
+    ]
 };
 
-var tsProject = ts.createProject("tsconfig.json");
-
 gulp.task("copy-html", function () {
-  return gulp.src(paths.pages)
-    .pipe(gulp.dest("dist"));
-});
-
-gulp.task("copy-css", function () {
-  return gulp.src(paths.css)
-    .pipe(gulp.dest("dist"));
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
 });
 
 gulp.task("copy-assets", function () {
-  return gulp.src(paths.assets)
-    .pipe(gulp.dest("dist"));
+    return gulp.src(paths.assets)
+        .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", gulp.series("copy-html", "copy-css", "copy-assets", function () {
-  return browserify({
-    basedir: '.',
-    debug: true,
-    entries: [
-      'src/main.ts'
-    ],
-    cache: {},
-    packageCache: {}
-  })
-    .plugin(tsify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest("dist"));
-  // return tsProject.src()
-  //   .pipe(tsProject())
-  //   .js.pipe(gulp.dest("dist"));
-}));
+gulp.task('build-sass', function () {
+    return gulp.src(paths.scss)
+        .pipe(sass({style: 'expanded'}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task("build-typescript", function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest("dist"));
+});
+
+
+gulp.task("watch", function () {
+    gulp.watch(paths.pages, gulp.series("copy-html"));
+    gulp.watch(paths.scss, gulp.series("build-sass"));
+    gulp.watch(paths.assets, gulp.series("copy-assets"));
+    gulp.watch(paths.typescript, gulp.series("build-typescript"));
+});
+
+gulp.task("default", gulp.series("copy-html", "build-sass", "copy-assets", "build-typescript", "watch"));
